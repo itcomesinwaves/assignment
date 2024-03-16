7f42901263bb9e6330b319f7c935f6b8e9beac77
Royce Taylor <royce.taylor789@gmail.com>
2024-03-16 01:48:28 -0500

    initial commit: rendering images as bordered checkboxes

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            options: {
+                implementation: require('sass'),
+                sourceMap: true, // Generate source maps
+                outputStyle: 'compressed' // Minify the CSS output
+            },
+            dist: {
+                files: {
+                    'dist/css/styles.css': 'src/scss/styles.scss'
+                }
+            }
+        },
+        concat: {
+            dist: {
+                src: ['src/js/*.js'],
+                dest: 'dist/js/script.js'
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    { expand: true, cwd: 'src/', src: ['index.html'], dest: 'dist/' }
+                ]
+            }
+        },
+        watch: {
+            css: {
+                files: ['src/scss/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    livereload: true,
+                }
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: ['concat'],
+                options: {
+                    livereload: true,
+                }
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['copy'],
+                options: {
+                    livereload: true,
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass'); // Load grunt-sass
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass', 'concat', 'copy', 'watch']);
+};
---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+const images = [
+    'https://acre-image-collections.s3.amazonaws.com/image-6578722caea701702392364_medium.jpg',
+    'https://acre-images.s3.amazonaws.com/image-5ac62b9e561041522936734_medium.jpeg',
+    'https://acre-images.s3.amazonaws.com/image-5acac631504581523238449_medium.jpeg',
+    'https://acre-image-collections.s3.amazonaws.com/image-65787222618211702392354_medium.jpg',
+    'https://acre-image-collections.s3.amazonaws.com/image-657872175b22e1702392343_medium.jpg',
+    'https://acre-image-collections.s3.amazonaws.com/image-657871ff7d8401702392319_medium.jpg',
+    'https://acre-image-collections.s3.amazonaws.com/image-6576286c7c4b61702242412_medium.jpg',
+    'https://acre-image-collections.s3.amazonaws.com/image-6578af5f9af671702408031_medium.jpg',
+    'https://acre-images.s3.amazonaws.com/image-5b01cdf4b2e9a1526844916.jpg',
+    'https://acre-image-collections.s3.amazonaws.com/image-6576286c7c4b61702242412_medium.jpg',
+];
+const moreImages = [...images];
+const allImages = [moreImages, images].flat();
+
+$(document).ready(function () {
+
+    const renderingImages = () => {
+        allImages.forEach((image, i) => {
+
+                $('.photo-grid').append(`
+                    <div loading="lazy" class="card">
+                        <input type="checkbox" id="checkbox${i}" />
+                        <label for="checkbox${i}" style="background-image: url('${image}');"></label>
+                    </div>
+                `);
+        });
+    };
+    renderingImages();
+});
